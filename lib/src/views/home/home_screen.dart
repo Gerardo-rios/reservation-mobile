8d87a405e5774b1fd1030a542869ca01700085bb
Gerardo Rios <jose.g.rios@unl.edu.ec>
2024-10-14 12:36:52 -0500

    Added home screen views

--- lib/src/views/home/home_screen.dart
+++ lib/src/views/home/home_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:sport_spot/src/constants/image_strings.dart';
-import 'package:sport_spot/src/controllers/auth_controller.dart';
+import 'package:sport_spot/src/controllers/home_screen_controller.dart';
+import 'package:sport_spot/src/views/home/history_page.dart';
+import 'package:sport_spot/src/views/home/home_page.dart';
+import 'package:sport_spot/src/views/home/profile_page.dart';
+import 'package:sport_spot/src/views/home/reservations_page.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -10,7 +13,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final AuthController authController = Get.find<AuthController>();
+    final HomeController homeController = Get.put(HomeController());
 
     return Scaffold(
       appBar: AppBar(
@@ -24,159 +27,38 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: IndexedStack(
-        children: <Widget>[
-          _buildHomePage(),
-          _buildReservationsPage(),
-          _buildHistoryPage(),
-          _buildProfilePage(authController),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_today),
-            label: 'Bookings',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.history),
-            label: 'History',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
-        onTap: (int index) {
-          // TODO: Implement navigation between pages
-        },
-      ),
+      body: Obx(() => IndexedStack(
+            index: homeController.selectedIndex.value,
+            children: const <Widget>[
+              HomePage(),
+              ReservationsPage(),
+              HistoryPage(),
+              ProfilePage(),
+            ],
+          )),
+      bottomNavigationBar: Obx(() => BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            currentIndex: homeController.selectedIndex.value,
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.calendar_today),
+                label: 'Bookings',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.history),
+                label: 'History',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: 'Profile',
+              ),
+            ],
+            onTap: homeController.changePage,
+          )),
     );
   }
-
-  Widget _buildHomePage() => ListView(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Search venues...',
-                prefixIcon: const Icon(Icons.search),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ),
-            ),
-          ),
-          _buildCategorySection(),
-          _buildPopularVenuesSection(),
-        ],
-      );
-
-  // TODO: Implement categories section
-  Widget _buildCategorySection() => SizedBox(
-        height: 100,
-        child: ListView(
-          scrollDirection: Axis.horizontal,
-          children: <Widget>[
-            _buildCategoryItem('Football', Icons.sports_soccer),
-            _buildCategoryItem('Basketball', Icons.sports_basketball),
-            _buildCategoryItem('Tennis', Icons.sports_tennis),
-            _buildCategoryItem('Swimming', Icons.pool),
-          ],
-        ),
-      );
-
-  Widget _buildCategoryItem(String title, IconData icon) => Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: <Widget>[
-            CircleAvatar(
-              radius: 30,
-              child: Icon(icon, size: 30),
-            ),
-            const SizedBox(height: 5),
-            Text(title),
-          ],
-        ),
-      );
-
-  // TODO: Implement popular venues section
-  Widget _buildPopularVenuesSection() => Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          const Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Text(
-              'Popular Venues',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-          ),
-          ListView.builder(
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            itemCount: 5,
-            itemBuilder: (BuildContext context, int index) => ListTile(
-              leading: const Icon(Icons.place),
-              title: Text('Venue ${index + 1}'),
-              subtitle: const Text('Brief description of the venue'),
-              trailing: ElevatedButton(
-                onPressed: () {
-                  // TODO: Implement booking action
-                },
-                child: const Text('Book'),
-              ),
-            ),
-          ),
-        ],
-      );
-
-  // TODO: Implement active bookings page
-  Widget _buildReservationsPage() => const Center(child: Text('Bookings Page'));
-
-  // TODO: Implement booking history page
-  Widget _buildHistoryPage() => const Center(child: Text('Booking History'));
-
-  Widget _buildProfilePage(AuthController authController) => ListView(
-        children: <Widget>[
-          const UserAccountsDrawerHeader(
-            accountName: Text('User Name'),
-            accountEmail: Text('user@example.com'),
-            currentAccountPicture: CircleAvatar(
-              backgroundImage: AssetImage(tAppLogo),
-            ),
-          ),
-          ListTile(
-            leading: const Icon(Icons.person),
-            title: const Text('Edit Profile'),
-            onTap: () {
-              // TODO: Implement profile editing
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.settings),
-            title: const Text('Settings'),
-            onTap: () {
-              // TODO: Implement settings
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.help),
-            title: const Text('Help & Support'),
-            onTap: () {
-              // TODO: Implement help and support
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.exit_to_app),
-            title: const Text('Log Out'),
-            onTap: authController.logout,
-          ),
-        ],
-      );
 }
